80d99621c0c756a3612dc307b05acd79cb4e674b
ckckckcz <rriovld@gmail.com>
2024-12-20 20:11:23 +0700

    update: portfolio data

---
+++ src/data/portfolio.ts
@@ -0,0 +1,11 @@
+import type { PortfolioData } from "../models/portfolio";
+
+export const projects: PortfolioData[] = [
+    {
+        title: "Cendekia Universitas Negeri Malang",
+        description: "Educational platform designed for the academic community, offering tools and resources to enhance learning experiences. It may serve as a digital space for students and faculty of Universitas Negeri Malang (UM), providing easy access to important materials and updates.",
+        image: "../src/assets/img/Portfolio/Web/CendekiaUM.png",
+        link: "https://cendekiaum.vercel.app",
+        label: "Landing Page"
+    },
+];
---
+++ src/models/portfolio.d.ts
@@ -0,0 +1,7 @@
+export interface PortfolioData {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+    label: string;
+}
