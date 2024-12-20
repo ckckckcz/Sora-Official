9fc74fa7178fac7fd4c63591aa615cfc44845484
ckckckcz <rriovld@gmail.com>
2024-12-20 20:46:47 +0700

    update: data

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -3,7 +3,7 @@ import { truncate } from "./functions/truncate";
 
 export const projects: PortfolioData[] = [
     {
-        title: "Cendekia Universitas Negeri Malang",
+        title: "Cendekia UM",
         description: truncate("Educational platform designed for the academic community, offering tools and resources to enhance learning experiences. It may serve as a digital space for students and faculty of Universitas Negeri Malang (UM), providing easy access to important materials and updates."),
         image: "../src/assets/img/Portfolio/Web/CendekiaUM.png",
         link: "https://cendekiaum.vercel.app",
